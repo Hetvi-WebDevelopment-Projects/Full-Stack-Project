f5b41e13772d11c40b16324c6301a10bbec75e50
Hetvi-WebDevelopment-Projects <hetvivaishnav0@gmail.com>
2026-05-06 21:57:21 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,64 @@
+{/*import cors from "cors";*/}
+const express = require('express')
+const cors=require('cors')
+const bycrpt= require('bcrypt')
+const mongoose= require('mongoose')
+const bodyParser= require('body-parser')
+
+const Event =require('./models/event')
+
+const app = express()
+
+
+ app.use(cors({
+    origin:" http://localhost:5173",
+}))
+app.use(express.json())
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/eventDBs')
+
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err))
+
+app.get("/", (req, res)=> {
+    res.send("API is working");
+});
+
+app.post('/submit' , async (req,res) => {
+    try{
+
+    const { name, email, eventName, date, location } = req.body
+    console.log("Incomming: ",req.body)
+    
+    if( !name || !email || !eventName || !date || !location ){
+        return res.status(400).json({ message: "All Fields must be completed"})
+    }   
+            const newEvent = new Event({
+                name,
+                email,
+                eventName, 
+                date,
+                location
+            })
+
+            await newEvent.save()
+
+            console.log("Saving to DB")
+
+            return res.status(200).json({message: "Event planned Sucessfully"})
+    }
+    catch(error){
+        console.error("Error:", error)
+        return res.status(500).json({message: "Error submitting form"})
+    }
+})
+
+mongoose
+    .connect("mongodb://localhost:27017/eventDBs")
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB error: ", err));
+
+app.listen(3000, ()=>{
+    console.log("Server Started on port 3000")
+})
---
+++ Frontend/major_project/src/App.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import './index.css';
+import { Routes, Route, Link} from "react-router-dom";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+import Service from "./Service";
+import Portfolio from "./Portfolio";
+import Plan from "./Plan";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App(){
+   return(
+    <>
+
+      <nav className="nav">
+      
+      <Link to="/" className="link">
+      Home
+      </Link>
+      
+      <Link to="/about" className="link">
+      About Us
+      </Link>
+
+     <Link to="/service" className="link">
+     Our Services
+     </Link>
+
+      <Link to="/portfolio" className="link">
+      Portfolio
+      </Link> 
+
+     </nav>
+
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/plan" element={<Plan />} />
+
+      </Routes>
+
+      <footer className="footer">
+  <h3>Outpro India</h3>
+  <p>Adventure & Sports Event Management Company</p>
+
+  <p>Email: info@outproindia.com</p>
+  <p>Phone: +91-XXXXXXXXXX</p>
+
+  <p>© 2026 Outpro India. All rights reserved.</p>
+</footer>
+    </>
+  )
+}
+
+export default App;
